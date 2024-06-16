e235e08b6907d38bf6a774e36971f48411ae708a
vince8x <vince.tran8x@gmail.com>
2024-06-16 09:33:24 +0700

    update neovim

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -2,5 +2,25 @@ return {
   {
     "Exafunction/codeium.vim",
     event = "BufEnter",
+    enabled = false,
+  },
+  {
+    "jackMort/ChatGPT.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("chatgpt").setup({
+        api_key_cmd = "pass show apikey/openai",
+        keymaps = {
+          submit = "<C-j>",
+          yank_last_code = "<C-y>",
+        },
+      })
+    end,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "folke/trouble.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
   },
 }
