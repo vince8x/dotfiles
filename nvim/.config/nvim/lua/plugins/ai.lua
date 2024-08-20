b1d60843223c91e4e5690643ef468fe40fff9207
vince8x <vince.tran8x@gmail.com>
2024-08-20 09:13:23 +0700

    Add plugin

--- nvim/.config/nvim/lua/plugins/ai.lua
+++ nvim/.config/nvim/lua/plugins/ai.lua
@@ -64,8 +64,8 @@ return {
     event = "VeryLazy",
     dependencies = {
       "vim-denops/denops.vim",
-      event = { 'VeryLazy' },
-      'vim-denops/denops-helloworld.vim'
+      event = { "VeryLazy" },
+      "vim-denops/denops-helloworld.vim",
     },
     keys = {
       { "<localleader>ar", "<cmd>AiderRun<cr>", mode = "n", noremap = true, silent = true },
@@ -139,6 +139,41 @@ return {
       "nvim-lua/plenary.nvim",
       "folke/trouble.nvim",
       "nvim-telescope/telescope.nvim",
+    },
+  },
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    build = "make",
+    config = {
+      provider = "deepseek"
+    },
+    keys = {
+      { "<leader>av", desc = "Toggle (Avante)" },
+      { "<leader>ar", desc = "Refresh (Avante)" },
+      { "<leader>ae", desc = "Edit (Avante)" },
+    },
+    opts = {
+      -- add any opts here
+    },
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      {
+        "grapp-dev/nui-components.nvim",
+        dependencies = {
+          "MunifTanjim/nui.nvim",
+        },
+      },
+      --- The below is optional, make sure to setup it properly if you have lazy=true
+      -- {
+      --   "MeanderingProgrammer/render-markdown.nvim",
+      --   opts = {
+      --     file_types = { "markdown", "Avante" },
+      --   },
+      --   ft = { "markdown", "Avante" },
+      -- },
     },
   },
   -- {
